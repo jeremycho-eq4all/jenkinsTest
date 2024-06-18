484a89ceb3abb00d7e7954e677a0b1e99c239494
jeremycho-eq4all <jeremycho@eq4all.co.kr>
2024-06-18 14:57:46 +0900

    qqq

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,7 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { expect } = chai;
 const server = require("../app");
+const { execSync } = require("child_process");
 
 chai.use(chaiHttp);
 
@@ -9,6 +10,17 @@ describe("GET /", () => {
   let app;
 
   before((done) => {
+    try {
+      // 기존 서버 종료
+      console.log("Try server to kill.");
+      execSync(
+        "lsof -i :3010 | grep LISTEN | awk '{print $2}' | xargs kill -9 || true"
+      );
+    } catch (e) {
+      console.log("No existing server to kill.");
+    }
+
+    // 새로운 서버 시작
     app = server.listen(3010, () => {
       console.log("Test server running on port 3010");
       done();
